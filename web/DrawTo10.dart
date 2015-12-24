da9efe61835cb100a2f89d57a35ffb68b4c293b8
miaoxiaole <11156216446@qq.com>
2015-12-24 13:22:31 +0800

    improve Timer

--- web/DrawTo10.dart
+++ web/DrawTo10.dart
@@ -4,11 +4,14 @@
 import 'DrawFrame.dart';
 import 'dart:async';
 import 'dart:math' as Math;
+import 'dart:html';
 class DrawTo10{
   DrawFrame drawFrame;
 
   var second;
   Timer timer;
+
+  // ImageElement img = new ImageElement();
 
   int number;
   int x;
@@ -20,12 +23,12 @@ class DrawTo10{
 
   DrawTo10(int conversion , var s ,var canvas){
     drawFrame = new DrawFrame();
-    second=new Duration(seconds:1);
+    second=new Duration(seconds:2);
     this.conversion =conversion;
     this.s =s;
     this.canvas = canvas;
     number =0;
-    x=70;y=70;
+    x=150;y=50;
 
   }
 
@@ -66,26 +69,115 @@ class DrawTo10{
 
     return num1;
   }
+
+
   void cal_to_10_draw(Timer _){
     var content = canvas.getContext('2d');
     content.font="bold 24px Times New Roman";
     content.fillStyle="grey";
     var length =drawFrame.getNumbers().length;
-    if(number<length){
-      content.fillText('+',x-15*2,y);
+    var width;
+    //img.src="num.png";
 
-      sum=sum+int.parse(drawFrame.getNumbers().elementAt(number))*Math.pow(drawFrame.getConversion(),length-1-number);
-      content.fillText('${drawFrame.getNumbers().elementAt(number)}',x,y);
-      content.fillText('×',x+15*2,y);
-      content.fillText('${drawFrame.getConversion()}'+'^'+'${length-1-number}',x+15*4,y);
-      content.fillText('——————',x,y+30);
-      //content.fillText('${sum}',x+30,y+60);
-      y=y+80;
-      number++;
+    // content.drawImage(img,100,100,111,157);
+    if(number<length){
+      if(number==0){
+        var temp=150;//number是整个数组中的第几个成员
+        sum=sum+int.parse(drawFrame.getNumbers().elementAt(number))*Math.pow(drawFrame.getConversion(),length-1-number);
+
+        for(var j=0;j<length;j++){
+          if(j==number){
+            content.fillStyle="blue";
+            content.fillText('${drawFrame.getNumbers().elementAt(j)}',temp-100,y);
+            temp=temp+15;
+          }
+          else{
+            content.fillStyle="grey";
+            content.fillText('${drawFrame.getNumbers().elementAt(j)}',temp-100,y);
+            temp=temp+15;
+          }
+        }
+        x=temp;
+
+        content.fillStyle="blue";
+        content.fillText('${drawFrame.getNumbers().elementAt(number)}',x,y);
+        content.fillStyle="grey";
+        content.fillText('×',x+15*2,y);
+        content.fillStyle="pink";
+        content.fillText('${drawFrame.getConversion()}'+'^'+'${length-1-number}',x+15*4,y);
+        y=y+50;
+        number++;
+
+      }else{
+        if(number<length-1){
+          var temp =150;
+
+          for(var j=0;j<length;j++){
+            if(j==number){
+              content.fillStyle="blue";
+              content.fillText('${drawFrame.getNumbers().elementAt(j)}',temp-100,y);
+              temp=temp+15;
+            }
+            else{
+              content.fillStyle="grey";
+              content.fillText('${drawFrame.getNumbers().elementAt(j)}',temp-100,y);
+              temp=temp+15;
+            }
+          }
+          x=temp;
+
+          content.fillText('+',x-15*2,y);
+
+          sum=sum+int.parse(drawFrame.getNumbers().elementAt(number))*Math.pow(drawFrame.getConversion(),length-1-number);
+          content.fillStyle="blue";
+          content.fillText('${drawFrame.getNumbers().elementAt(number)}',x,y);
+          content.fillStyle="grey";
+          content.fillText('×',x+15*2,y);
+          content.fillStyle="pink";
+          content.fillText('${drawFrame.getConversion()}'+'^'+'${length-1-number}',x+15*4,y);
+          y=y+50;
+          number++;
+
+        }
+        else{   //所有值的最后一列
+          var temp =150;
+
+          for(var j=0;j<length;j++){
+            if(j==number){
+              content.fillStyle="blue";
+              content.fillText('${drawFrame.getNumbers().elementAt(j)}',temp-100,y);
+              temp=temp+15;
+            }
+            else{
+              content.fillStyle="grey";
+              content.fillText('${drawFrame.getNumbers().elementAt(j)}',temp-100,y);
+              temp=temp+15;
+            }
+          }
+          x=temp;
+
+          content.fillStyle="grey";
+          content.fillText('+',x-15*2,y);
+          content.fillStyle="blue";
+          sum=sum+int.parse(drawFrame.getNumbers().elementAt(number))*Math.pow(drawFrame.getConversion(),length-1-number);
+          content.fillText('${drawFrame.getNumbers().elementAt(number)}',x,y);
+          content.fillStyle="grey";
+          content.fillText('×',x+15*2,y);
+          content.fillStyle="pink";
+          content.fillText('${drawFrame.getConversion()}'+'^'+'${length-1-number}',x+15*4,y);
+          content.fillStyle="grey";
+          content.fillText('——————',x-15*2,y+25);
+          content.fillStyle="black";
+          content.fillText('${sum}',x+60,y+50);
+          number++;
+        }
+
+      }
+
     }else
     {
       timer.cancel();
-      x=70;y=70;
+      x=150;y=150;
       number =0;
       sum =0;
     }
